5c71df6e0ee1b90f4753959d3686151210e24bb8
BengiOnMAC <356844587@qq.com>
2019-12-26 15:22:01 +0800

    Init springboot project

---
+++ src/main/java/com/atcn/bdd/ui/mobile/testautomationuimobile/TestAutomationUiMobileApplication.java
@@ -0,0 +1,13 @@
+package com.atcn.bdd.ui.mobile.testautomationuimobile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAutomationUiMobileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestAutomationUiMobileApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/atcn/bdd/ui/mobile/testautomationuimobile/TestAutomationUiMobileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atcn.bdd.ui.mobile.testautomationuimobile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestAutomationUiMobileApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
